6893b318323a46f7c475744b75deb7f2f2d9178a
NMarkgraf <nmarkgraf@hotmail.com>
2025-08-26 17:22:32 +0200

    setup.R mit etwas mehr Informationen

--- Workshop/setup/setup.R
+++ Workshop/setup/setup.R
@@ -1,1 +1,6 @@
+#
+# Diese R-Bibliotheken/Pakete sollten Sie vorab installiert haben.
+# Sie können das einfach machen, in dem Sie diese Datei ausführen.
+# Zum Beispiel mit Hilfe des Source Knopfes oben rechts!
+#
 install.packages(c("knitr", "rmarkdown", "tidyverse", "DBI", "odbc", "here", "lubridate", "palmerpenguins", "nycflights13", "RSQLite", "RPostgreSQL", "janitor", "plotly"))
