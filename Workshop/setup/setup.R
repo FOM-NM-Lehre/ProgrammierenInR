03eb8119aab6512e003d3a6fea17c0b7e0bb9128
NMarkgraf <nmarkgraf@hotmail.com>
2025-08-28 09:25:12 +0200

    Kleinere Updates

--- Workshop/setup/setup.R
+++ Workshop/setup/setup.R
@@ -3,4 +3,9 @@
 # Sie können das einfach machen, in dem Sie diese Datei ausführen.
 # Zum Beispiel mit Hilfe des Source Knopfes oben rechts!
 #
-install.packages(c("knitr", "rmarkdown", "tidyverse", "DBI", "odbc", "here", "lubridate", "palmerpenguins", "nycflights13", "RSQLite", "RPostgreSQL", "janitor", "plotly"))
+install.packages(c(
+    "knitr", "rmarkdown", "quarto",
+    "tidyverse",  "here", "lubridate", "janitor", "plotly", "sessioninfo",
+    "DBI", "odbc"," RSQLite", "RPostgreSQL",
+    "palmerpenguins", "nycflights13",
+))
